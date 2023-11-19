85161baab6c99bf9943d38e66c70a1c02e88d389
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2023-11-19 12:58:11 +0530

    Add user registration API endpoint

--- app.js
+++ app.js
@@ -25,3 +25,30 @@ const initializeDbAndServer = async()=>{
 }
 initializeDbAndServer();
 
+//regiser new user
+// api 1
+app.post("/register/",async(request,response)=>{
+    const {username,password,name,gender}=request.body;
+    const checkUser=`SELECT username from user WHERE username='${username}'`;
+    const dbUser = await db.get(checkUser);
+    if(dbUser!==undefined){
+        response.status(400);
+        response.send("User already exists");
+    }
+    else{
+        if(password.length<6){
+            response.status(400);
+            response.send("Password is too short");
+
+        }
+        else{
+            const hashedPassword = await bcrypt.hash(password,10);
+            const createUserQuery = `INSERT INTO user(name,username,password,gender) values(
+                '${name}','${username}','${password}','${name}','${gender}')`;
+            await db.run(createUserQuery);
+            response.status(200);
+            response.send("User created successfully");
+        }
+    }
+
+});
