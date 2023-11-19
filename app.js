4c08c8f53c251619d092f75a668db727180feab9
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2023-11-19 13:06:48 +0530

    Refactor code for initializing database and server

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
 const express = require("express");
-const app = express();
-const {open} = require("sqlite");
-const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 const path = require("path");
-const dbPath = path.join(__dirname,"twitterClone.db");
-let db = null;
-app.use(express.json());
+const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+//const format = require("date-fns/format");
+let database;
+const app = express();
+app.use(express.json());
 
-const initializeDbAndServer = async()=>{
-    try{
-        db = await open({
-            filename:dbPath,
-            driver:sqlite3.Database
-        });
-        app.listen(3000,()=>{
-            console.log("Server running at http://localhost:3000/");
-        });
-    }catch(e){
-        console.log(`DB Error:${e.message}`);
-        process.exit(1);
-    }
-}
-initializeDbAndServer();
+const initializeDBandServer = async () => {
+	try {
+		database = await open({
+			filename: path.join(__dirname, "twitterClone.db"),
+			driver: sqlite3.Database,
+		});
+		app.listen(3000, () => {
+			console.log("Server is running on http://localhost:3000/");
+		});
+	} catch (error) {
+		console.log(`Database error is ${error.message}`);
+		process.exit(1);
+	}
+};
 
-//homepage 
+initializeDBandServer();
 
-app.get("/",(request,response)=>{
+//homepage
+
+app.get("/", (request, response) => {
     response.send("Twitter Clone");
 }
 );
 
-//regiser new user
-// api 1
-app.post("/register/",async(request,response)=>{
-    const {username,password,name,gender}=request.body;
-    const checkUser=`SELECT username from user WHERE username='${username}'`;
-    const dbUser = await db.get(checkUser);
-    if(dbUser!==undefined){
-        response.status(400);
-        response.send("User already exists");
-    }
-    else{
-        if(password.length<6){
-            response.status(400);
-            response.send("Password is too short");
 
-        }
-        else{
-            const hashedPassword = await bcrypt.hash(password,10);
-            const createUserQuery = `INSERT INTO user(name,username,password,gender) values(
-                '${name}','${username}','${password}','${name}','${gender}')`;
-            await db.run(createUserQuery);
-            response.status(200);
-            response.send("User created successfully");
-        }
-    }
+//api 1
+
+app.post("/register/", async (request, response) => {
+	const { username, password, name, gender } = request.body;
+	const checkUser = `select username from user where username='${username}';`;
+	const dbUser = await database.get(checkUser);
+	console.log(dbUser);
+	if (dbUser !== undefined) {
+		response.status(400);
+		response.send("User already exists");
+	} else {
+		if (password.length < 6) {
+			response.status(400);
+			response.send("Password is too short");
+		} else {
+			const hashedPassword = await bcrypt.hash(password, 10);
+			const requestQuery = `insert into user(name, username, password, gender) values(
+          '${name}','${username}','${hashedPassword}','${gender}');`;
+			await database.run(requestQuery);
+			response.status(200);
+			response.send("User created successfully");
+		}
+	}
 });
