f743aef8751980ac408036178d4da5fbbb98c510
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2023-11-20 11:55:41 +0530

    Add authentication middleware and API endpoint for user tweets feed

--- app.js
+++ app.js
@@ -88,3 +88,53 @@ app.post("/login/",async(request,response)=>{
 );
 
 
+
+//authentication middleware
+
+const authenticationToken =(request,response,next)=>{
+	let jwtToken;
+	const authHeader=request.headers["authorization"];
+	if(authHeader!==undefined){
+		jwtToken=authHeader.split(" ")[1];
+	}
+	else{
+		response.status(401);
+		response.send("Invalid JWT Token");
+	}
+
+	if(jwtToken!=undefined){
+		jwt.verify(jwtToken,"MY_SECRET_TOKEN",async(error,payload)=>{
+			if(error){
+				response.status(401);
+				response.send("Invalid JWT Token");
+			}
+			else{
+				request.username=payload.username;
+				next();
+			}
+		});
+
+	}
+}
+
+//api 3
+
+app.get("/user/tweets/feed/", authenticationToken, async (request, response) => {
+	let { username } = request;
+	const getUserIdQuery = `select user_id from user where username='${username}';`;
+	const getUserId = await database.get(getUserIdQuery);
+	// console.log(getUserId);
+	const getFollowerIdsQuery = `select following_user_id from follower 
+    where follower_user_id=${getUserId.user_id};`;
+	const getFollowerIdsArray = await database.all(getFollowerIdsQuery);
+	// console.log(getFollowerIdsArray);
+	const getFollowerIds = getFollowerIdsArray.map((eachUser) => {
+		return eachUser.following_user_id;
+	});
+	// console.log(`${getFollowerIds}`);
+	const getFollowersResultQuery = `select name from user where user_id in (${getFollowerIds});`;
+	const responseResult = await database.all(getFollowersResultQuery);
+	//console.log(responseResult);
+	response.send(responseResult);
+});
+
